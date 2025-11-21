a4f4c54743355cb8c69a409fae35dcfaca7f0ed7
Vladan Sekulic <is231338@fhstp.ac.at>
2025-11-22 00:28:49 +0100

    Refactor convert_condition_not method
    Refactor convert_condition_not to handle string conversion and leading whitespace.

--- sigma/backends/logpoint/logpoint.py
+++ sigma/backends/logpoint/logpoint.py
@@ -273,7 +273,7 @@ class Logpoint(TextQueryBackend):
             self.modify_condition_from_json_value_construction(cond)
         return super().convert_condition(cond, state)
 
-    def convert_condition_not(
+  def convert_condition_not(
         self, cond: ConditionNOT, state: ConversionState
     ) -> Union[str, DeferredQueryExpression]:
         """Conversion of NOT conditions."""
@@ -282,12 +282,15 @@ class Logpoint(TextQueryBackend):
             if (
                 arg.__class__ in self.precedence
             ):  # group if AND or OR condition is negated
-                return self.not_token + self.convert_condition_group(arg, state)
+                grouped = self.convert_condition_group(arg, state)
+                return self.not_token + grouped.lstrip()
+            
             expr = self.convert_condition(arg, state)
             if isinstance(
                 expr, DeferredQueryExpression
             ):  # negate deferred expression and pass it to parent
                 return expr.negate()
-            return self.not_token + expr  # convert negated expression to string
+                
+            return self.not_token + str(expr).lstrip() 
         except TypeError:  # pragma: no cover
             raise NotImplementedError("Operator 'not' not supported by the backend")
